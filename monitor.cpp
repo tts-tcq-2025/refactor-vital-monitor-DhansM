199ce009aaffcabf168a8b5f350dc6d4cf392da2
DhansM <31100015+DhansM@users.noreply.github.com>
2025-08-22 09:59:52 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -38,28 +38,42 @@ using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
   return 1;
 }*/
 
+
 void showAlert(const std::string& message) {
-  cout << message << "\n";
-  for (int i = 0; i < 6; i++) {
-    cout << "\r* " << flush;
-    sleep_for(seconds(1));
-    cout << "\r *" << flush;
-    sleep_for(seconds(1));
-  }
+    cout << message << "\n";
+    for (int i = 0; i < 6; i++) {
+        cout << "\r* " << flush;
+        this_thread::sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        this_thread::sleep_for(seconds(1));
+    }
+}
+
+bool isTemperatureOk(float temperature) {
+    return temperature >= 95 && temperature <= 102;
+}
+
+bool isPulseRateOk(float pulseRate) {
+    return pulseRate >= 60 && pulseRate <= 100;
+}
+
+bool isSpo2Ok(float spo2) {
+    return spo2 >= 90;
 }
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    showAlert("Temperature is critical!");
-    return 0;
-  }
-  if (pulseRate < 60 || pulseRate > 100) {
-    showAlert("Pulse Rate is out of range!");
-    return 0;
-  }
-  if (spo2 < 90) {
-    showAlert("Oxygen Saturation out of range!");
-    return 0;
-  }
-  return 1;
+    if (!isTemperatureOk(temperature)) {
+        showAlert("Temperature is critical!");
+        return 0;
+    }
+    if (!isPulseRateOk(pulseRate)) {
+        showAlert("Pulse Rate is out of range!");
+        return 0;
+    }
+    if (!isSpo2Ok(spo2)) {
+        showAlert("Oxygen Saturation out of range!");
+        return 0;
+    }
+    return 1;
 }
+
