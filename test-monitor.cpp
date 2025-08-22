a5ace288630557821c1fd44e57cc83f46dc20b87
DhansM <31100015+DhansM@users.noreply.github.com>
2025-08-22 09:49:22 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -4,4 +4,7 @@
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_FALSE(vitalsOk(99, 102, 70));
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+  ASSERT_TRUE(vitalsOk(90, 50, 105));
+  ASSERT_TRUE(vitalsOk(72, 99, 95));
 }
+
