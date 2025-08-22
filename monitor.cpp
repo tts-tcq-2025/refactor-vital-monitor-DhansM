eb94989f7394a6021a868c1de06287e2fb438611
DhansM <31100015+DhansM@users.noreply.github.com>
2025-08-22 10:59:03 +0530

    Update monitor_8.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -6,7 +6,6 @@
 #include <string>
 #include <vector>
 #include <algorithm> 
-
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
 /*int vitalsOk(float temperature, float pulseRate, float spo2) {
